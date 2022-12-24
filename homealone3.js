c145a04e29e20eda1187a460be6f10c6e5efc891
tayyab javed <tayyabdev@outlook.com>
2022-12-24 04:50:28 +0000

    done

---
+++ homealone3.js
@@ -0,0 +1,48 @@
+
+const homealone1 = "rtmp://talkfssteam.com:1936/live/homealone1";
+
+/**
+ * 
+ * ffmpeg -re -nostdin -i Homealone1.mp4 \
+ -vcodec libx264 -preset:v ultrafast \
+     -acodec aac \
+     -f flv rtmp://talkfssteam.com:1936/live/homealone1
+ * 
+ */
+
+const child_process = require('child_process');
+
+function startStream(){
+const ffmpeg = child_process.spawn('ffmpeg', [
+    '-re',
+    '-nostdin',
+    '-i',
+    'https://maeplet.fra1.digitaloceanspaces.com/homealone/homealone3.mp4',
+    '-vcodec',
+    'libx264',
+    '-preset:v',
+    'ultrafast',
+    '-acodec',
+    'aac',
+    '-f',
+    'flv',
+    'rtmp://talkfssteam.com:1936/live/homealone3'
+]);
+
+ffmpeg.on('close', (code) => {
+    console.log("stream over===================================")
+    setTimeout(()=>{
+        startStream()
+    },10000)
+
+});
+
+ffmpeg.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+});
+}
+
+startStream();
+
+
+
